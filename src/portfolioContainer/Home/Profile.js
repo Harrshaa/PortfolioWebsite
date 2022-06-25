db185332a435bb803a334a1e2a560efbc9c061c7
Harsha <sriharsha.mutakodoor@gmail.com>
2022-06-25 18:31:19 +0530

    add icons

---
+++ src/portfolioContainer/Home/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+    <div className='profile-parent'>
+    <div className='profile-details'>
+    <div className='colz'>
+
+    <a href='https://www.notion.so/REACT-63d7d3a504d94969a951e3f515f82772'>
+    <i className="bi bi-linkedin"></i>
+    </a>
+    <a href='https://www.notion.so/REACT-63d7d3a504d94969a951e3f515f82772'>
+    <i className="fa fa-instagram"></i>
+    </a>
+    <a href='https://www.notion.so/REACT-63d7d3a504d94969a951e3f515f82772'>
+    <i className="fa fa-twitter"></i>
+    </a>
+    
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
